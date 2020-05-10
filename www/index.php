34938a477a15acae8f4f5260552c8d2d39bb0405
Øyvind Hatland <oyvind.hatland@yahoo.com>
2020-05-10 05:26:21 +0200

    connects to database and displays query on page

---
+++ www/index.php
@@ -0,0 +1,43 @@
+<?php
+$databaseDockername = "db";
+$username = "root";
+$password = "test";
+$dbname = "fileStorage";
+
+// this example uses table new_table with colloms id and name
+
+// PDO statements https://www.php.net/manual/en/class.pdostatement.php
+
+try {
+    $conn = new PDO("mysql:host=$databaseDockername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully <br>";
+    
+    $stmt = $conn->prepare("SELECT * FROM new_table");
+    $stmt->execute();
+
+    // set the resulting array to numeric 
+    $result = $stmt->setFetchMode(PDO::FETCH_NUM);
+    
+    // for table new_table with colloms id and name
+    while ($row = $stmt->fetch()) {
+        print $row[0] . " - " . $row[1] . "<br>";
+    }
+
+    /* Alternative
+    
+    // set the resulting array to associative 
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    
+    // for table new_table with colloms id and name
+    while ($row = $stmt->fetch()) {
+        print $row['id'] . "\t" . $row['name'] . "\n";
+    }
+    */
+
+
+} catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+?>
