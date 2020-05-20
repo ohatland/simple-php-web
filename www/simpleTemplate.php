e78d3c224d183323033900a94a18e61d3d29b0a2
Øyvind Hatland <oyvind.hatland@yahoo.com>
2020-05-20 16:00:41 +0200

    moved template to testTemplate folder

--- www/simpleTemplate.php
+++ www/simpleTemplate.php
@@ -1,10 +1,10 @@
 <?php 
     require_once '/var/www/vendor/autoload.php';
 
-    $loader = new \Twig\Loader\FilesystemLoader('/var/www/html/templates');
+    $loader = new \Twig\Loader\FilesystemLoader('/var/www/html/testTemplates');
     $twig = new \Twig\Environment($loader);
 
-    $template = $twig->load('simpleTemplate.html');
+    $template = $twig->load('simpleTemplate.twig');
 
-    echo $template->render(['title' => 'My first Twig', 'name' => 'Øyvind']);
+    echo $template->render(['title' => 'My first Twig', 'name' => 'Erlend']);
 ?>
