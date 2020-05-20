0007eeaae3334e22b1f255fb8e11462c4dc06623
Zathoici <erlend58@hotmail.com>
2020-05-20 05:36:06 +0200

    Added test data with sp's
    Co-authored-by: Øyvind Hatland <hatland.oyvind@gmail.com>

--- dbInit/03_procedures.sql
+++ dbInit/03_procedures.sql
@@ -2,7 +2,7 @@ USE recipies;
 
 
 DELIMITER $$
-CREATE PROCEDURE sp_InsertRecipie (IN recipieName VARCHAR(150), userID INT, OUT recipieID)
+CREATE PROCEDURE sp_InsertRecipie (IN recipieName VARCHAR(150), userID INT, OUT recipieID INT)
 BEGIN
 SET @currentDatetime = CURRENT_TIMESTAMP();
 INSERT INTO recipie (recipieName, owner, createdBy, created, alteredBy, altered)
@@ -45,7 +45,7 @@ CREATE PROCEDURE sp_InsertIngredient (IN recipieID INT, sequenceNumber INT, user
 BEGIN
 CALL sp_InsertRecipieSeqence(recipieID, sequenceNumber, userID, @recipieSequenceID);
 SET @currentDatetime = CURRENT_TIMESTAMP();
-INSERT INTO image (recipieID, recipieSequenceID, ingredientName, ammount, owner, createdBy, created, alteredBy, altered)
+INSERT INTO ingredient (recipieID, recipieSequenceID, ingredientName, ammount, owner, createdBy, created, alteredBy, altered)
 VALUES (recipieID, @recipieSequenceID, ingredientName, ammount, userID, userID, @currentDatetime, userID, @currentDatetime);
 END
 $$
@@ -53,12 +53,12 @@ DELIMITER ;
 
 DELIMITER $$
 
-CREATE PROCEDURE sp_InsertRecipeText (IN recipieID INT, sequenceNumber INT, userID INT, recipieText VARCHAR(10000))
+CREATE PROCEDURE sp_InsertRecipieText (IN recipieID INT, sequenceNumber INT, userID INT, recipieText VARCHAR(10000))
 BEGIN
 CALL sp_InsertRecipieSeqence(recipieID, sequenceNumber, userID, @recipieSequenceID);
 SET @currentDatetime = CURRENT_TIMESTAMP();
 INSERT INTO recipieText (recipieID, recipieSequenceID, recipieText, owner, createdBy, created, alteredBy, altered)
-VALUES (recipieID, @recipieSequenceID, imagePath, imageTitle, userID, userID, @currentDatetime, userID, @currentDatetime);
+VALUES (recipieID, @recipieSequenceID, recipieText, userID, userID, @currentDatetime, userID, @currentDatetime);
 END
 $$
 DELIMITER ;
@@ -69,7 +69,7 @@ CREATE PROCEDURE sp_InsertStep (IN recipieID INT, sequenceNumber INT, userID INT
 BEGIN
 CALL sp_InsertRecipieSeqence(recipieID, sequenceNumber, userID, @recipieSequenceID);
 SET @currentDatetime = CURRENT_TIMESTAMP();
-INSERT INTO image (recipieID, recipieSequenceID, stepText, owner, createdBy, created, alteredBy, altered)
+INSERT INTO step (recipieID, recipieSequenceID, stepText, owner, createdBy, created, alteredBy, altered)
 VALUES (recipieID, @recipieSequenceID, stepText, userID, userID, @currentDatetime, userID, @currentDatetime);
 END
 $$
@@ -77,11 +77,58 @@ DELIMITER ;
 
 DELIMITER $$
 
-CREATE PROCEDURE sp_SelectRecipie (IN recipieID INT)
+CREATE PROCEDURE sp_SelectRecipie (IN recipie_ID INT)
 BEGIN
 
-SELECT * FROM recipieInSequence WHERE recipieID = recipieID;
+SELECT * FROM recipieInSequence WHERE recipieID = recipie_ID;
 
 END
 $$
 DELIMITER ;
+
+DELIMITER $$
+
+CREATE PROCEDURE sp_SelectAllRecipies ()
+BEGIN
+
+SELECT recipieName, owner, ID FROM recipie
+ORDER BY recipieName;
+
+END
+$$
+DELIMITER ;
+
+
+CALL sp_InsertRecipie("Brownies", 1, @recipieID);
+CALL sp_InsertIngredient(@recipieID, 1, 1, "Mel", "300 gram");
+CALL sp_InsertIngredient(@recipieID, 2, 1, "Sukker", "200 gram");
+CALL sp_InsertIngredient(@recipieID, 3, 1, "Kanel", "10 gram");
+CALL sp_InsertIngredient(@recipieID, 4, 1, "Lime Jucie", "2 SS");
+CALL sp_InsertStep(@recipieID,5,1, "Steg 1");
+CALL sp_InsertStep(@recipieID,6,1, "Steg 2");
+CALL sp_InsertStep(@recipieID,7,1, "Steg 3");
+CALL sp_InsertRecipieText(@recipieID,8,1, "Beskrivelse av oppskrift");
+CALL sp_InsertImage(@recipieID,9,1, "C:/windows/malware.exe", "Malware");
+
+CALL sp_InsertRecipie("Kake", 1, @recipieID);
+CALL sp_InsertIngredient(@recipieID, 1, 1, "Mel", "300 gram");
+CALL sp_InsertIngredient(@recipieID, 2, 1, "Sukker", "200 gram");
+CALL sp_InsertIngredient(@recipieID, 3, 1, "Kanel", "10 gram");
+CALL sp_InsertIngredient(@recipieID, 4, 1, "Lime Jucie", "2 SS");
+CALL sp_InsertStep(@recipieID,5,1, "Steg 1");
+CALL sp_InsertStep(@recipieID,6,1, "Steg 2");
+CALL sp_InsertStep(@recipieID,7,1, "Steg 3");
+CALL sp_InsertRecipieText(@recipieID,8,1, "Beskrivelse av oppskrift");
+CALL sp_InsertImage(@recipieID,9,1, "C:/windows/malware.exe", "Malware");
+
+CALL sp_InsertRecipie("Saft", 1, @recipieID);
+CALL sp_InsertIngredient(@recipieID, 1, 1, "Vann", "3 liter");
+CALL sp_InsertIngredient(@recipieID, 2, 1, "Sukker", "200 gram");
+CALL sp_InsertIngredient(@recipieID, 3, 1, "Appelsin Jucie", "5 dl");
+CALL sp_InsertIngredient(@recipieID, 4, 1, "Lime Jucie", "2 SS");
+CALL sp_InsertStep(@recipieID,5,1, "Steg 1");
+CALL sp_InsertStep(@recipieID,6,1, "Steg 2");
+CALL sp_InsertStep(@recipieID,7,1, "Steg 3");
+CALL sp_InsertRecipieText(@recipieID,8,1, "Beskrivelse av oppskrift");
+CALL sp_InsertImage(@recipieID,9,1, "C:/windows/malware.exe", "Malware");
+
