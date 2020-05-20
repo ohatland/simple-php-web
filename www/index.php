c50cc14db78abbd6bfffe540f1dddf8f201e355d
Øyvind Hatland <oyvind.hatland@yahoo.com>
2020-05-20 17:45:33 +0200

    add select all recipies

--- www/index.php
+++ www/index.php
@@ -1,14 +1,18 @@
 <?php 
+    include './classes/recipiesDB.php';
+    $recipiesDB = new recipiesDB();
+    
     require_once '/var/www/vendor/autoload.php';
-
     $loader = new \Twig\Loader\FilesystemLoader(['/var/www/html/layoutTemplates', '/var/www/html/recipieTemplates']);
     $twig = new \Twig\Environment($loader);
 
     
     echo $twig->render('head.twig', ['title' => 'Main Page']);
     echo $twig->render('header.twig');
-
+    
     // content goes here
+    $dbResult = $recipiesDB->getAllRecipies();
+    echo $twig->render('showAll.twig', ['recipies' => $dbResult]);
     
     echo $twig->render('footer.twig');
 
