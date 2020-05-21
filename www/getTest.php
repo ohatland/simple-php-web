6745bde7ecf520ff9bb578fd6d2db194ac3b81ba
Øyvind Hatland <oyvind.hatland@yahoo.com>
2020-05-21 20:37:10 +0200

    a example of how to recive get request

---
+++ www/getTest.php
@@ -0,0 +1,34 @@
+<?php 
+    include './classes/recipie.php';
+    $recipie = new Recipie();
+    
+    require_once '../vendor/autoload.php';
+    $loader = new \Twig\Loader\FilesystemLoader(['./layoutTemplates', './recipieTemplates']);
+
+    if( $_GET["recipie"]) {
+        $twig = new \Twig\Environment($loader);
+
+    
+        echo $twig->render('head.twig', ['title' => 'Main Page']);
+        echo $twig->render('header.twig');
+        
+        // content goes here
+        $dbResult = $recipie->getRecipie($_GET['recipie']);
+    
+        echo $twig->render('showRecipie.twig', ['recipies' => $dbResult]);
+        
+        echo $twig->render('footer.twig');
+        exit();
+     }
+
+?>
+<html>
+   <body>
+   
+      <form action = "<?php $_PHP_SELF ?>" method = "GET">
+         Recipie number: <input type = "number" name = "recipie" />
+         <input type = "submit" />
+      </form>
+      
+   </body>
+</html>
