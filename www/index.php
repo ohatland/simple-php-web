8a91abcbf3375326436489571a12a8373c0fc10a
Øyvind Hatland <oyvind.hatland@yahoo.com>
2020-05-22 15:28:35 +0200

    render get request from index file to index file

--- www/index.php
+++ www/index.php
@@ -1,19 +1,29 @@
 <?php 
+    include './classes/httpResponse.php';
     include './classes/recipie.php';
     $recipie = new Recipie();
     
     require_once '../vendor/autoload.php';
     $loader = new \Twig\Loader\FilesystemLoader(['./layoutTemplates', './recipieTemplates']);
-    $twig = new \Twig\Environment($loader);
-
     
+
+    if( $_GET["recipie"]) {
+        // render the selected recipie
+        getRecipie($loader, $recipie);
+    }
+
+    // else if $_POST
+    
+    // else
+    // Render a overview of available recipies
+    $twig = new \Twig\Environment($loader);
     echo $twig->render('head.twig', ['title' => 'Main Page']);
     echo $twig->render('header.twig');
     
-    // content goes here
     $dbResult = $recipie->getAllRecipies();
     echo $twig->render('showAll.twig', ['recipies' => $dbResult]);
     
     echo $twig->render('footer.twig');
+
 
 ?>
