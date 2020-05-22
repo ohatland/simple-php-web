23f2542a74418cf7fe90acd22f82772494fed90d
Øyvind Hatland <oyvind.hatland@yahoo.com>
2020-05-22 15:38:24 +0200

    fixed indentation

--- www/classes/httpResponse.php
+++ www/classes/httpResponse.php
@@ -2,22 +2,22 @@
 
 function getRecipie($loader, $recipie)
 {
-   $twig = new \Twig\Environment($loader);
+    $twig = new \Twig\Environment($loader);
 
- 
-   echo $twig->render('head.twig', ['title' => 'Main Page']);
-   echo $twig->render('header.twig');
+    
+    echo $twig->render('head.twig', ['title' => 'Main Page']);
+    echo $twig->render('header.twig');
 
-   $dbResult = $recipie->getAllRecipies();
-   echo $twig->render('showAll.twig', ['recipies' => $dbResult]);
-   
-   // content goes here
-   $dbResult = $recipie->getRecipie($_GET['recipie']);
-   
-   echo $twig->render('showRecipie.twig', ['recipies' => $dbResult]);
-   
-   echo $twig->render('footer.twig');
-   exit();
+    $dbResult = $recipie->getAllRecipies();
+    echo $twig->render('showAll.twig', ['recipies' => $dbResult]);
+    
+    // content goes here
+    $dbResult = $recipie->getRecipie($_GET['recipie']);
+    
+    echo $twig->render('showRecipie.twig', ['recipies' => $dbResult]);
+    
+    echo $twig->render('footer.twig');
+    exit();
 }
 
 function postRecipie()
