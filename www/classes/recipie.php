46f2296ebd5ac522a40667b70c965c2893ba06e7
Zathoici <erlend58@hotmail.com>
2020-05-23 08:06:11 +0200

    added insert methods

--- www/classes/recipie.php
+++ www/classes/recipie.php
@@ -19,4 +19,40 @@ class Recipie extends DB
         $stmt->execute();
         return $stmt->fetchAll();
     }
+
+    public function newRecipie($recipieName, $userID)
+    {
+        $sql = "CALL spInsertRecipie($recipieName, $userID)";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+
+    public function addRecipieText($recipieID, $sequenceNumber, $userID, $recipieText)
+    {
+        $sql = "CALL spInsertRecipie($recipieID, $sequenceNumber, $userID, $recipieText)";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute();
+    }
+
+    public function addIngredient($recipieID, $sequenceNumber, $userID, $ingredientName, $ammount)
+    {
+        $sql = "CALL spInsertRecipie($recipieID, $sequenceNumber, $userID, $ingredientName, $ammount)";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute();
+    }
+
+    public function addStep($recipieID, $sequenceNumber, $userID, $stepText)
+    {
+        $sql = "CALL spInsertRecipie($recipieID, $sequenceNumber, $userID, $stepText)";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute();
+    }
+
+    public function addImage($recipieID, $sequenceNumber, $userID, $imagePath, $imageTitle)
+    {
+        $sql = "CALL spInsertRecipie($recipieID, $sequenceNumber, $userID, $imagePath, $imageTitle)";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute();
+    }
 }
