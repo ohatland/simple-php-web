b713dea4526d9229565e6d5cae34641565763cb5
Zathoici <erlend58@hotmail.com>
2020-05-24 02:28:34 +0200

    readying for mvc

--- www/index.php
+++ www/index.php
@@ -1,5 +1,5 @@
 <?php 
-    include './classes/httpResponse.php';
+    
     include './classes/recipe.php';
     $recipe = new recipe();
     
@@ -7,22 +7,10 @@
     $loader = new \Twig\Loader\FilesystemLoader(['./layoutTemplates', './recipeTemplates']);
     
 
-    if( $_GET["recipe"]) {
-        // render the selected recipe
-        getrecipe($loader, $recipe);
-    }
-
-    // else if $_POST
     
-    // else
-    // Render a overview of available recipes
     $twig = new \Twig\Environment($loader);
     echo $twig->render('head.twig', ['title' => 'Main Page']);
     echo $twig->render('header.twig');
-    
-    $dbResult = $recipe->getAllrecipes();
-    echo $twig->render('showAllRecipes.twig', ['recipes' => $dbResult]);
-    
     echo $twig->render('footer.twig');
 
 ?>
