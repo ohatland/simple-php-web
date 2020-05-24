9324d6b284e5f2b7801bfb47c8b1d398701473de
Zathoici <erlend58@hotmail.com>
2020-05-24 09:45:12 +0200

    added loginUser, newUser and hashPassword methods

--- www/classes/user.php
+++ www/classes/user.php
@@ -1,9 +1,32 @@
 <?php
-include 'db.php'
+include 'db.php';
 
-class user extends db
+class User extends DB
 {
+    function loginUser()
+    {
+        $userMail = $_POST['userName'];
+        $password = hashPassword($_POST['userPassword']);
+        // TODO make stored procedure
+        $sql = "CALL sp_LoginUser(?, ?)";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$userMail, $password]);
+        $result = $stmt->fetchAll();
+        $_SESSION['userID'] = $result['ID'];
+    }
 
+    function newUser()
+    {
+        // TODO Check to see if user exists
+        $sql = "CALL sp_InsertUser(?, ?, ?, ?)";
+        $stmt = $this->connect()-prepare($sql);
+        $stmt->execute([$_POST['firstName'], $_POST['lastName'], $_POST['userMail'], hashPassword($_POST['password'])]);        
+    }
+    
+    private function hashPassword($passwordInn)
+    {
+        return password_hash($passwordInn, PASSWORD_DEFAULT);
+    }
 }
 
 ?>
