25c7e01d545cc996420c9f3a079fa1f7279b3398
Zathoici <erlend58@hotmail.com>
2020-05-24 09:43:38 +0200

    added userTemplates to file loader

--- www/index.php
+++ www/index.php
@@ -1,12 +1,14 @@
 <?php 
     require_once '../vendor/autoload.php';
-    $loader = new \Twig\Loader\FilesystemLoader(['./layoutTemplates', './recipeTemplates']);
+    $loader = new \Twig\Loader\FilesystemLoader(['./layoutTemplates', './recipeTemplates', './userTemplates']);
     
 
     
     $twig = new \Twig\Environment($loader);
     echo $twig->render('head.twig', ['title' => 'Main Page']);
+    
     echo $twig->render('header.twig');
+    echo $twig->render('formNewUser.twig');
     echo $twig->render('footer.twig');
 
 ?>
