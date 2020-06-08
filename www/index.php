3fe17bc7c74072cb2843477c1a041a75e7e832f8
Zathoici <erlend58@hotmail.com>
2020-06-08 20:42:53 +0200

    added login functionality

--- www/index.php
+++ www/index.php
@@ -1,14 +1,19 @@
 <?php 
+    require_once 'classes/user.php';
+
+    session_start();
+    $user = new User();
+    if (isset($_POST['login']))
+    {
+        $user->loginUser();
+    }
     require_once '../vendor/autoload.php';
     $loader = new \Twig\Loader\FilesystemLoader(['./layoutTemplates', './recipeTemplates', './userTemplates']);
-    
-
-    
     $twig = new \Twig\Environment($loader);
-    echo $twig->render('head.twig', ['title' => 'Main Page']);
-    
-    echo $twig->render('header.twig');
-    echo $twig->render('formNewUser.twig');
+    echo $twig->render('header.twig', ['title' => 'Main Page']);
+    if ($_SESSION['userID'])
+        echo $twig->render('index.twig');
+    else 
+        echo $twig->render('formLogin.twig');
     echo $twig->render('footer.twig');
-
 ?>
