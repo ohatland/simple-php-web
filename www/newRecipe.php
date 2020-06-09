dc2df24ca31f5d49fd20407103b0ee1be33702a3
Øyvind Hatland <oyvind.hatland@yahoo.com>
2020-06-10 01:42:18 +0200

    render given recipe and buttons to add steps to that recipe

--- www/newRecipe.php
+++ www/newRecipe.php
@@ -1,3 +1,37 @@
 <?php
-    echo "Test2";
+    include 'header.php';
+    require_once 'classes/recipe.php';
+
+    $recipie = new Recipe();
+    
+    // get working recipe
+    $dbResult = $recipie->getRecipe($_GET['recipe']);
+    echo $twig->render('showRecipe.twig', ['recipes' => $dbResult]);
+    
+    
+    
+    // option on adding new element
+    echo $twig->render('showAllButtonCreateForm.twig');
+    
+    if (isset($_POST['image']))
+    {
+        echo $twig->render('formRecipeImage.twig');
+    }
+
+    elseif (isset($_POST['ingredient']))
+    {
+        echo $twig->render('formRecipeIngredient.twig');
+    }
+
+    elseif (isset($_POST['step']))
+    {
+        echo $twig->render('formRecipeStep.twig');
+    }
+
+    elseif (isset($_POST['text']))
+    {
+        echo $twig->render('formRecipeText.twig');
+    }
+    
+    include 'footer.php';
 ?>
