32eda8e6b4eec7ad3dc8295942425c58a3cf3b31
Zathoici <erlend58@hotmail.com>
2020-06-09 22:28:21 +0200

    moved things to header and footer, included signout functionality

--- www/index.php
+++ www/index.php
@@ -1,19 +1,12 @@
 <?php 
-    require_once 'classes/user.php';
+    include 'header.php';
 
-    session_start();
-    $user = new User();
-    if (isset($_POST['login']))
-    {
-        $user->loginUser();
-    }
-    require_once '../vendor/autoload.php';
-    $loader = new \Twig\Loader\FilesystemLoader(['./layoutTemplates', './recipeTemplates', './userTemplates']);
-    $twig = new \Twig\Environment($loader);
-    echo $twig->render('header.twig', ['title' => 'Main Page']);
+    if (isset($_POST['signOut']))
+        $user->signOut();
     if ($_SESSION['userID'])
         echo $twig->render('index.twig');
     else 
         echo $twig->render('formLogin.twig');
-    echo $twig->render('footer.twig');
+    
+    include 'footer.php';
 ?>
