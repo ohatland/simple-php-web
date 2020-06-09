a7b6e64a3d63b69c4890a6c5a0c7569fe9fcb3e2
Zathoici <erlend58@hotmail.com>
2020-06-10 00:03:08 +0200

    added render

--- www/myRecipes.php
+++ www/myRecipes.php
@@ -1,3 +1,13 @@
 <?php
-    echo "Test";
+    include 'header.php';
+    require_once 'classes/recipe.php';
+
+    $recipie = new Recipe();
+    
+    $dbResult = $recipie->getUserRecipes();
+
+    print_r($dbResult);
+    echo $twig->render('showAllRecipes.twig', ['recipies' => $dbResult]);
+    include 'footer.php';
+
 ?>
