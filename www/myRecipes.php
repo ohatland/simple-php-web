3c24706e30a404322c0c379e2ad5404129a3ef38
Zathoici <erlend58@hotmail.com>
2020-06-10 00:35:24 +0200

    made it so recipes render next to the list of recipes when clicked

--- www/myRecipes.php
+++ www/myRecipes.php
@@ -2,11 +2,17 @@
     include 'header.php';
     require_once 'classes/recipe.php';
 
-    $recipie = new Recipe();
+    $recipe = new Recipe();
     
-    $dbResult = $recipie->getUserRecipes();
+    $dbResult = $recipe->getUserRecipes();
 
     echo $twig->render('showAllRecipes.twig', ['recipes' => $dbResult]);
+
+    if (isset($_GET['recipe']))
+    {
+        $recipeResult = $recipe->getRecipe($_GET['recipe']);
+        echo $twig->render('showRecipe.twig', ['recipes' => $recipeResult]);
+    }
     include 'footer.php';
 
 ?>
